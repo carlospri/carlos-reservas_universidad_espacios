77e076888fcaf91568cf594ab70b74d8d436df1d
carlospri <carlospri55@gmail.com>
2025-02-27 15:04:07 -0500

    se agrego un cotrolador modelo y migracion

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReserveController;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('home');
@@ -12,3 +13,4 @@ Route::get('/', function () {
 
 Route::resource('Home', HomeController::class);
 Route::resource('Reserve', ReserveController::class);
+Route::resource('Products', ProductsController::class);
